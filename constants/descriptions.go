ba0474f7bdd2950301f6c21627a619ebefcec1fc
prakhar <pi.prakhar.dev@gmail.com>
2025-04-18 18:58:18 +0530

    feature: watch logs description correction

--- constants/descriptions.go
+++ constants/descriptions.go
@@ -5,7 +5,7 @@ const (
 	CommonFlagDescDeploymentNames = "List of deployment names (required)"
 	CommonFlagDescPodNames        = "List of pod names (required)"
 	CommonFlagDescTag             = "Image tag to deploy (required)"
-	CommonFlagDescLocation        = "Location to the file"
+	CommonFlagDescLocation        = "Location to the file (default: current directory)"
 
 	CommonFlagDescWatchFrequency = "Frequency of fetching %s in seconds"
 )
